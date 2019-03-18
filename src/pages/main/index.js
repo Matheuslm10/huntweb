13e77ed11dbf308fc6291cd073580ffc83492926
Matheus Machado <matheus_lm10@hotmail.com>
2019-03-18 18:27:08 -0400

    progresso

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from '../../services/api';
 
+import "./styles.css";
+
 export default class Main extends Component {
     state = {
         products: []
@@ -16,6 +18,23 @@ export default class Main extends Component {
     };
 
     render(){
-        return <h1>Contagem de produtos: {this.state.products.length}</h1>;
+        const {products} = this.state;
+
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+                <div className="actions">
+                    <button>Anterior</button>
+                    <button>Próxima</button>
+                </div>
+            </div>
+        )
     }
 }
